f20df80f5e9d1ad7d1c3f4139ae33802d6fef542
Mohit Gupta <mogupta@ecfmg.org>
2021-07-21 11:54:27 -0400

    added hostname in graphql query1

--- graphql/resolvers/queries.ts
+++ graphql/resolvers/queries.ts
@@ -1,3 +1,12 @@
+import os from 'os'
 export const query1=async() => {
-    return 'query successful'
+    let h = process.env.WEBSITE_SITE_NAME
+    let s = os.hostname()
+    if(!h){
+        return s
+    }
+    else{
+        return h
+    }
+    // return 'query successful'
 }
